6383193c2618109aabae309f116cef591c05803d
AristoDevloper <aryancodes200@gmail.com>
2026-08-07 19:08:54 +0545

    fix: remove duplicate setting of DJANGO_SETTINGS_MODULE in asgi.py

--- Ecommerce_Site_Backend/asgi.py
+++ Ecommerce_Site_Backend/asgi.py
@@ -9,14 +9,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce_Site_Backend.settings.production')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from EcomApp.routing import websocket_urlpatterns
 from EcomApp.middleware import JWTAuthMiddlewareStack
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce_Site_Backend.settings.production')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
