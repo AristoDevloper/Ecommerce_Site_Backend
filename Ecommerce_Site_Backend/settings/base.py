e4d84dba2e21e31783ebe06d45c31e57c2cb9a11
AristoDevloper <aryancodes200@gmail.com>
2026-08-08 02:07:20 +0545

    feat: add Whitenoise middleware for static file handling and update requirements

--- Ecommerce_Site_Backend/settings/base.py
+++ Ecommerce_Site_Backend/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -72,6 +73,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
--- Ecommerce_Site_Backend/wsgi.py
+++ Ecommerce_Site_Backend/wsgi.py
@@ -14,3 +14,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce_Site_Backend.settings
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
+app = application
