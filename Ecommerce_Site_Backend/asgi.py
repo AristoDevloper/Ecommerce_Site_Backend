d3a830b005a87db00273a591a97833083de16280
AristoDevloper <aryancodes200@gmail.com>
2026-08-07 20:00:29 +0545

    fix: store ASGI application in a variable for reuse in routing configuration

--- Ecommerce_Site_Backend/asgi.py
+++ Ecommerce_Site_Backend/asgi.py
@@ -14,11 +14,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce_Site_Backend.settings
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+
+django_asgi_app = get_asgi_application()
+
 from EcomApp.routing import websocket_urlpatterns
 from EcomApp.middleware import JWTAuthMiddlewareStack
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         JWTAuthMiddlewareStack(
             URLRouter(
