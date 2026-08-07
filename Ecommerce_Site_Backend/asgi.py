9b814c30e21d14b30cf60714f670e4d731e30e2c
AristoDevloper <aryancodes200@gmail.com>
2026-08-07 20:10:41 +0545

    fix: update DJANGO_SETTINGS_MODULE to use production settings in asgi.py

--- Ecommerce_Site_Backend/asgi.py
+++ Ecommerce_Site_Backend/asgi.py
@@ -9,16 +9,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce_Site_Backend.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce_Site_Backend.settings.production')
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 
 django_asgi_app = get_asgi_application()
 
 from EcomApp.routing import websocket_urlpatterns
 from EcomApp.middleware import JWTAuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
